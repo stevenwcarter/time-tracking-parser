9d29880e4cf7a00a0f5649f3d15d8ea677c1608a
Steven Carter <steve@javapl.us>
2025-08-02 23:33:11 -0400

    feat: Adding convenience format methods

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,31 @@ impl TimeTrackingData {
     pub fn from_json(json: &str) -> Result<Self, serde_json::Error> {
         serde_json::from_str(json)
     }
+
+    pub fn formatted_start_time(&self) -> String {
+        self.start_time
+            .as_ref()
+            .map_or("N/A".to_string(), format_time)
+    }
+
+    pub fn formatted_end_time(&self) -> String {
+        self.end_time
+            .as_ref()
+            .map_or("N/A".to_string(), format_time)
+    }
+
+    pub fn formatted_total_minutes(&self) -> String {
+        Time::format_duration_minutes(self.total_minutes)
+    }
+    pub fn formatted_dead_time_minutes(&self) -> String {
+        Time::format_duration_minutes(self.dead_time_minutes)
+    }
+    pub fn formatted_total_decimal(&self) -> String {
+        Time::format_duration_decimal(self.total_minutes)
+    }
+    pub fn formatted_dead_decimal(&self) -> String {
+        Time::format_duration_decimal(self.dead_time_minutes)
+    }
 }
 
 /// Parse a time string like "7:30" or "7"
